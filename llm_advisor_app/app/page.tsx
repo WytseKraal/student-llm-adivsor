df080ea064416a64425ec4d7b78fc764e59711f9
wytse <wytse.kraal@student.uva.nl>
2025-02-18 23:46:44 +0100

    deploy test

--- llm_advisor_app/app/page.tsx
+++ llm_advisor_app/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button onClick={handleButtonClick}>Hallo</Button>
+      <Button onClick={handleButtonClick}>I have DEPLOYED</Button>
       {showText && <p className="text-cyan-800 text-3xl mt-4">I AM CLICKED</p>}
     </div>
   );
