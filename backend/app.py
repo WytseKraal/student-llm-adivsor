32831c6cef5929a228f2445f89bace8fa7adb49c
wytse <wytse.kraal@student.uva.nl>
2025-02-19 14:14:15 +0100

    cors in code

--- backend/app.py
+++ backend/app.py
@@ -3,5 +3,10 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+        },
         'body': json.dumps({'message': 'hello'})
     }
