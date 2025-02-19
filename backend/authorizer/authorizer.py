73087755d922bc8b91df226ad4690654c56a84bd
wytse <wytse.kraal@student.uva.nl>
2025-02-19 15:34:36 +0100

    add logging

--- backend/authorizer/authorizer.py
+++ backend/authorizer/authorizer.py
@@ -1,10 +1,21 @@
+
+import logging
+import json
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
+    logger.info("Received event: %s", json.dumps(event))
     token = event.get("headers", {}).get("x-custom-auth")
     expected_token = "TEST_AUTH_KEY" 
 
     if token == expected_token:
+        logger.info("Token matched, allow")
         return generate_policy("user", "Allow", event["methodArn"])
     else:
+        logger.info(f"Token denied: {token}")
         return generate_policy("user", "Deny", event["methodArn"])
 
 
