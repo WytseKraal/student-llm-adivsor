f8b6c20d1ce7fb0502da40bae4adc9f37d592c92
wytse <wytse.kraal@student.uva.nl>
2025-02-23 22:18:16 +0100

    fix body

--- backend/models/event.py
+++ backend/models/event.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Dict, Optional
 from pydantic import BaseModel, Field, ValidationError
 
 class LambdaEvent(BaseModel):
     httpMethod: str
     headers: dict[str, str] = Field(default_factory=dict)
     path: str = ""
+    queryStringParameters: Optional[Dict[str, str]] = None
     body: Optional[str] = None
