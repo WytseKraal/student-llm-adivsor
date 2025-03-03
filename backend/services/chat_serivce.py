3057ceaaf4670dd0878c3d161fef711db90d34f3
MitchellWeg <mitchell.w@live.nl>
2025-03-03 21:05:29 +0100

    implement llm

--- backend/services/chat_serivce.py
+++ backend/services/chat_serivce.py
@@ -2,12 +2,17 @@ import openai
 import os
 import json
 import logging
+import boto3
+from boto3.dynamodb.conditions import Key
 from services.base_service import BaseService, APIError
 from models.response import LambdaResponse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+REGION = 'eu-north-1'
+TABLENAME = 'application_database'
+STUDENT = 'STUDENT#10002'
 
 class ChatService(BaseService):
     def __init__(self, event, context):
@@ -44,10 +49,22 @@ class ChatService(BaseService):
                 raise APIError("Missing 'message' in request body",
                                status_code=400)
 
+            enrollments = self.get_items_sk_begins_with(STUDENT, 'ENROLLMENT')
+            grades = self.get_items_sk_begins_with(STUDENT, 'RESULT')
+            profile = self.get_items_sk_begins_with(STUDENT, 'PROFILE')
+            timetable = self.get_items_sk_begins_with(STUDENT, 'TIMETABLE')
 
             prompt = f"""
             You are an expert student helper.
-            Help this student as best as you can with this message
+            The profile of this student is: {profile}
+
+            The grades of this student is: {grades}
+
+            This student is enrolled in: {enrollments}
+
+            This students time table is: {timetable}
+
+            Help this student as best as you can with this message, but don't tell them that their grades are low if they are.
 
             {user_message}
             """
@@ -81,3 +98,28 @@ class ChatService(BaseService):
         except Exception as e:
             self.logger.error(f"Error processing OpenAI request: {str(e)}")
             raise APIError("Internal server error", status_code=500)
+
+    def fetch_student_items(self, pk_value):
+        dynamodb = boto3.resource('dynamodb', region_name=REGION)
+        table = dynamodb.Table(TABLENAME)
+        response = None
+        try:
+            response = table.query(KeyConditionExpression=Key('pk').eq(pk_value))
+        except Exception as e:
+            print(f"Error fetching items for {pk_value}: {e}")
+
+        return response
+
+
+    def get_items_sk_begins_with(self, pk_value, sk_prefix):
+        dynamodb = boto3.resource('dynamodb', region_name=REGION)
+        table = dynamodb.Table(TABLENAME)
+        try:
+            response = table.query(
+                KeyConditionExpression=Key('pk').eq(pk_value) & Key('sk').begins_with(sk_prefix)
+            )
+            items = response.get('Items', [])
+            return items
+        except Exception as e:
+            print(f"Error fetching items for {pk_value} with sk prefix {sk_prefix}: {e}")
+            return None
